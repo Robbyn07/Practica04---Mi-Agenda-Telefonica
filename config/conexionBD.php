7f4217cd955a0815e4b396a814a40d9aec9f09f9
Robbyn07 <robbyn_reyes@hotmail.com>
2020-05-24 09:07:24 -0500

    Ingreso de estilos y actualizacion del proyecto.

---
+++ config/conexionBD.php
@@ -0,0 +1,17 @@
+<?php
+
+    $db_servername = "localhost";
+    $db_username = "root";
+    $db_password = "";
+    $db_name = "practica4";
+
+    $conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
+    $conn -> set_charset("utf8");
+
+    #Probar conexion
+    if($conn -> connect_error){
+        die("Connection failed:" . $conn -> connect_error);
+    }else{
+        echo "<p>Conexion exitosa!!</p>";
+    }
+?>
